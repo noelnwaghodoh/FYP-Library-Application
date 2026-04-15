95f020a09e77458f5dd251f5828490e4618ca879
noelnwaghodoh <monstershq@outlook.com>
2026-04-15 16:54:02 +0100

    fix: made fixes to last commit

--- src/app/(pages)/student/catalogue/components/book-display.js
+++ src/app/(pages)/student/catalogue/components/book-display.js
@@ -9,11 +9,11 @@ export function BookDisplay({ book }) {
   const bookThumbnailName = "thumb+" + parts[0];
 
   // Default fallbacks referencing your wireframe format
-  const year = book?.BookYear || "YYYY";
+  const year = book?.BookDate|| "YYYY";
   const title = book?.BookTitle || "Book Title 1";
   const author = book?.BookAuthor || "Author";
   const description = book?.ItemDescription || "This is placeholder text for the synopsis of the book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s";
-  
+  const location = book?.Location || "This is a placeholder location";
 
     function handleReadClick() {
       const fileName = book?.BookFileName || "";
@@ -55,7 +55,7 @@ export function BookDisplay({ book }) {
         <div className="flex flex-row gap-5 items-stretch">
           
           {/* Thumbnail Box */}
-          <div className="flex justify-center items-center w-[120px] h-[120px] border-[1.5px] rounded-md bg-white flex-shrink-0 p-2">
+          <div className="flex justify-center items-center w-[120px] h-[120px] border-[1.5px]  rounded-md bg-white flex-shrink-0 p-2">
              <Image
                 src={`https://fyp-assets.lon1.cdn.digitaloceanspaces.com/thumbnails/${bookThumbnailName}.jpg`}
                 width={120}
